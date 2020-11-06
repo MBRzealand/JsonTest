006d85575a3e5012466506e3aee6a95f631e487f
MBRzealand <mikk24f6@edu.easj.dk>
2020-11-06 15:29:58 +0100

    selectItem() method added

--- src/Example2/Main.java
+++ src/Example2/Main.java
@@ -2,30 +2,60 @@ package Example2;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Scanner;
 
 public class Main{
 
-    static String jsonStr = JsonItemString.getJsonData();
+    static void selectItem() throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Scanner input = new Scanner(System.in);
+
+        String jsonStr = JsonItemString.getJsonData();
+
+
+        ItemObject itemObject = objectMapper.readValue(jsonStr, ItemObject.class);
+
+
+        boolean searchingForItem = true;
+
+
+        while (searchingForItem) {
+
+            System.out.println("Input Item Name:");
+
+            String searchedItem = input.nextLine();
+
+
+                Item fetchedItem = itemObject.getItems().stream()
+                        .filter(x -> searchedItem.equals(x.getItemName()))
+                        .findAny().orElse(null);
+
+
+                if(fetchedItem != null) {
+
+                    searchingForItem = false;
+
+                    System.out.println(fetchedItem.printInfo());
+
+                } else {
+                    System.out.println("Item Doesn't exist");
+                }
+
+            }
+
+    }
+
+
+
+
 
     public static void main(String[] args) throws JsonProcessingException {
 
-
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        ItemObject testObject = objectMapper.readValue(jsonStr, ItemObject.class);
-
-
-
-        Item result1 = testObject.getItems().stream()
-                .filter(x -> "Cotton Tabard".equals(x.getItemName()))
-                .findAny()
-                .orElse(null);
-
-        assert result1 != null;
-        System.out.println(result1.printInfo());
-
+        selectItem();
 
     }
+
+
 
 }
